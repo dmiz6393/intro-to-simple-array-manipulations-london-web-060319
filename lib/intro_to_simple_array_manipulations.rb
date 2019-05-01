d192101a66e6ccb2bdeac31307566a2142d9dbba
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-01 18:09:11 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,34 @@
+def using_push(array,string)
+  array.push(string)
+end 
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end 
+
+using pop_with_args(array)
+#using pop_with_args(array)
+array.pop(2)
+end 
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  #shift_with_args takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array
+  
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array,element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  
